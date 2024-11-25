2bdb6e7ad225ff5403daa465ad060d9a4d5c925c
Simon Pythoud <simon.pythoud@swissmarketplace.group>
2024-11-25 14:19:16 +0100

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { LanguageProvider } from './contexts/LanguageContext';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders language switcher', () => {
+  render(
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
+  );
+  const languageButton = screen.getByText('EN');
+  expect(languageButton).toBeInTheDocument();
 });
