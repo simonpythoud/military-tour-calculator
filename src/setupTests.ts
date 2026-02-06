c48ef27e4e5b094ffc99d56a584865f92739904c
Simon Pythoud <simonpythoud@gmail.com>
2026-02-06 11:19:28 +0000

    Fix all npm vulnerabilities: upgrade jspdf to v4, typescript, and add npm overrides

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Polyfill TextEncoder/TextDecoder for jspdf v4 compatibility in Jest
+import { TextEncoder, TextDecoder } from 'node:util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder as typeof global.TextDecoder;
